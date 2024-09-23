4167790c9a4a342c098c9b5954c861e3d4770ef5
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-23 21:36:43 +0600

    string

---
+++ slice.js
@@ -0,0 +1,9 @@
+const address = 'tuhinmahmud';
+const part = address.slice(2, 5)
+// console.log(part);
+const debuge = 'i am a good person';
+// const bug = debuge.split(' ')
+// console.log(bug);
+
+// const part2 = address.split('m')
+// console.log(part2);
