81a6cff6feb31435cc3d08616a520ca4937b7ff1
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-23 22:06:33 +0600

    slice include concat split join added

--- slice.js
+++ slice.js
@@ -1,5 +1,5 @@
 const address = 'tuhinmahmud';
-const part = address.slice(2, 5)
+// const part = address.slice(2, 5)
 // console.log(part);
 const debuge = 'i am a good person';
 // const bug = debuge.split(' ')
@@ -7,3 +7,18 @@ const debuge = 'i am a good person';
 
 // const part2 = address.split('m')
 // console.log(part2);
+
+const frends = ['mahmud', 'hasan', 'tuhin', 'tawsif']
+
+// console.log(frends);
+// const part = frends.join(' ')
+// console.log(part);
+// const name1 = 'tuhin';
+// const name2 = 'mahmud';
+
+// const firstName = 'tuihn'
+// const firstName1 = 'mahmud'
+// // console.log(firstName + ' ' + firstName1);
+// console.log(firstName.concat(' ').concat(firstName1));
+const name3 = 'tuhin mahmud'
+console.log(name3.includes('h')); 
