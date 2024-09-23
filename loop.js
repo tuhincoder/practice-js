c298e7a7b3a9a99978cac9e247e2941f81a2fc86
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 00:54:54 +0600

    for of and for in use practical case

--- loop.js
+++ loop.js
@@ -1,5 +1,6 @@
 const mobile = {
     name: 'i phone 12 max pro',
+    price: 1500000,
     brand: 'apple',
     color: 'black',
     camera: '63 me',
@@ -7,5 +8,12 @@ const mobile = {
 
 }
 for (const prop in mobile) {
-    console.log(prop);
+    // console.log(prop);
+}
+
+const keys = Object.keys(mobile)
+console.log(keys);
+for (const key of keys) {
+    // console.log(key);
+    console.log(key, mobile[key]);
 }
