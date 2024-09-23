0c4847f7ff3323c0aa9cf4e326afcc5c0dbd52d6
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 00:43:21 +0600

    loop object working

---
+++ loop.js
@@ -0,0 +1,11 @@
+const mobile = {
+    name: 'i phone 12 max pro',
+    brand: 'apple',
+    color: 'black',
+    camera: '63 me',
+    isNew: true
+
+}
+for (const prop in mobile) {
+    console.log(prop);
+}
