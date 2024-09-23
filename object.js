67e02c4d849878fcf9eda468def30283bd3f5f23
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-23 23:48:33 +0600

    object file added

---
+++ object.js
@@ -0,0 +1,67 @@
+// const person = {
+//     name: 'tuhin',
+//     age: 25,
+//     profession: 'web developer',
+//     salary: 20000,
+//     married: true
+// }
+// console.log(person);
+// console.log(person['salary']);
+
+// const profession = person['age']
+// console.log(profession);
+
+// const person = {
+//     name: 'tuhin',
+//     age: 25,
+//     profession: 'web developer',
+//     salary: 20000,
+//     married: true
+// }
+// const person = {
+//     name: 'tuhin',
+//     age: 25,
+//     profession: 'web developer',
+//     salary: 20000,
+//     "full_name": 'mahmudul hasan  tuhin',
+//     married: true
+// }
+
+// console.log(person.salary);
+// console.log(person['age']);
+// console.log(person['name']);
+// person['salary'] = 400000;
+// console.log(person);
+
+// person['age'] = 40;
+// console.log(person);
+
+// console.log(person['full_name']);
+// const fullName = person['full_name']
+// console.log(fullName);
+
+
+// person['full_name'] = ['tuhin mahmudulllll']
+// console.log(person);
+
+// person['married'] = false;
+// console.log(person);
+const person = {
+    name: 'tuhin',
+    age: 25,
+    profession: 'web developer',
+    salary: 20000,
+    "full_name": 'mahmudul hasan  tuhin',
+    married: true
+}
+
+// const probName = 'profession'
+// person[probName] = 'softwar engineer'
+// console.log(person);
+
+// person['profession'] = 'front-end developer';
+// console.log(person);
+// person['age'] = 50
+// console.log(person);
+// person.salary = 600000;
+// console.log(person);
