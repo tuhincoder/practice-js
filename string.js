c3ff783360bc0204071cb40f2885e17ec77164f4
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-23 10:55:25 +0600

    uppercase and trim

--- string.js
+++ string.js
@@ -5,3 +5,21 @@ const numbers = [12, 34, 55, 34, 67, 45, 78, 34]
 
 // numbers[5] = 3000000;
 // console.log(numbers);
+
+
+const subject1 = 'chemistry '
+const subject2 = ' Chemistry'
+
+// if (subject1 === subject2) {
+//     console.log('yes this match the subject');
+// }
+// else {
+//     console.log('not match');
+// }
+
+if (subject1.trim() === subject2.toLocaleLowerCase().trim()) {
+    console.log('this subject for me ');
+}
+else {
+    console.log('this subject not for me');
+}
