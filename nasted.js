9a77ca48bcc262f0a444e68a096a3d846927a675
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 00:09:15 +0600

    object work running

--- nasted.js
+++ nasted.js
@@ -11,3 +11,13 @@ const collage = {
     }
 
 }
+// console.log(collage.class[1]);
+// console.log(collage.events[3]);
+collage.events[3] = '26 december'
+// console.log(collage.events[3]);
+// console.log(collage.events);
+// collage.unique.result.merit = 'top top top merit list'
+// console.log(collage);
+// collage.unique.color = 'red'
+// console.log(collage);
+console.log(collage['unique'].result.gpa);
