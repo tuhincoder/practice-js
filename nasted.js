34e945f74e4ee669bdeaabaff51824b421c28c9a
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-23 23:56:57 +0600

    nested file added

---
+++ nasted.js
@@ -0,0 +1,13 @@
+const collage = {
+    name: 'international',
+    class: ['11', '12'],
+    events: ['science', 'bijoy dibosh', 'shadinota dibosh', '21 feb'],
+    unique: {
+        color: 'blue',
+        result: {
+            gpa: 5,
+            merit: 'top'
+        }
+    }
+
+}
