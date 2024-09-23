dcfd9a6adca7121fec600966d2bace3d73bf08c3
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 02:26:53 +0600

    another looping

--- loop.js
+++ loop.js
@@ -15,5 +15,5 @@ const keys = Object.keys(mobile)
 console.log(keys);
 for (const key of keys) {
     // console.log(key);
-    console.log(key, mobile[key]);
+    console.log(key, ':', mobile[key]);
 }
---
+++ manyLooping.js
@@ -0,0 +1,27 @@
+const friends = ['elon', 'bill', 'mark', 'waren']
+
+// for (const friend of friends) {
+//     console.log(friend);
+// }
+
+for (let i = 0; i < friends.length; i++) {
+    // console.log(friends[i], i);
+}
+let i = 0;
+while (i < friends.length) {
+    // console.log(friends[i]);
+    i++;
+}
+
+
+const numbers = [23, 4, 34, 34, 56, 34, 654, 24, 34, 64]
+for (let i = 0; i < numbers.length; i++) {
+    // console.log(numbers[i]);
+}
+
+let n = 0;
+while (n < numbers.length) {
+    console.log(numbers[n]);
+    n++;
+}
+
