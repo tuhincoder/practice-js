51d83604c33d50a02e9b8748a612e11148c1293b
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 09:31:50 +0600

    sort for loop for of for in added

--- revers.js
+++ revers.js
@@ -1,6 +1,6 @@
 const numbers = [23, 4, 34, 34, 56, 34, 654, 24, 34, 64]
 // console.log(numbers);
-numbers.reverse()
+// numbers.reverse()
 // console.log(numbers);
 
 // ==================
@@ -15,8 +15,70 @@ numbers.reverse()
 
 // console.log(revers_num);
 
-const revers_num = [];
-for (const num of numbers) {
-    revers_num.unshift(num)
+// const revers_num = [];
+// for (const num of numbers) {
+//     revers_num.unshift(num)
+// }
+// console.log(revers_num);
+
+// const revers_num = []
+// for (let i = 0; i < numbers.length; i++) {
+//     const num = numbers[i]
+//     console.log(num);
+//     revers_num.unshift(num)
+// }
+// console.log(numbers);
+const numbers2 = [23, 4, 34, 34, 56, 34, 654, 24, 34, 64]
+// numbers2.reverse()
+// console.log(numbers2);
+// const rever = numbers2.reverse()
+// console.log(rever);
+
+for (let i = 0; i < numbers2.length; i++) {
+
 }
-console.log(revers_num);
+
+// for (const num of numbers2) {
+//     console.log(num);
+// }
+// const revers_num = [];
+// for (const num of numbers2) {
+//     // console.log(num);
+//     revers_num.push(num)
+// }
+// console.log(revers_num);
+
+// const revers_num = [];
+// for (const num of numbers2) {
+//     revers_num.unshift(num)
+// }
+// console.log(revers_num);
+const names = ['elon', 'bil', 'jakar', 'alibava']
+
+// for (const nam of names) {
+//     // console.log(nam);
+// }
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(i);
+//     console.log(names[i]);
+// }
+
+// let i =0;
+// while(i < names.length){
+//     console.log(i);
+//     console.log(names[i]);
+//     i++;
+// }
+
+const reversed = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(reversed);
+// const rev = reversed.reverse()
+// console.log(rev);
+// const sorted = reversed.sort()
+// console.log(sorted);
+
+const names2 = ['elon', 'bil', 'jakar', 'alibava']
+
+const sorted = names2.sort()
+console.log(sorted);
